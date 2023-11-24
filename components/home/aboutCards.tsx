43472e975fed2d3c283240d9f9792b39c33d8371
YeongMeng <makyeongmeng@gmail.com>
2023-11-24 15:14:03 +0800

    Section 2 Skeleton

--- components/home/aboutCards.tsx
+++ components/home/aboutCards.tsx
@@ -1,9 +1,33 @@
-const AboutCards = () => {
+import Image from "next/image";
+
+interface AboutProps {
+    title:string,
+    description:string,
+    button:string,
+    image:string
+}
+
+const AboutCards = ({title, description, button, image}:AboutProps) => {
     return ( 
-        <div>
-            <h1>
-                
+        <div className="w-full border-2 rounded-2xl h-full p-5 text-xl flex flex-col">
+            <h1 className="text-center text-2xl">
+                {title}
             </h1>
+            <Image
+                src={require("../../public/background.jpg")}
+                alt={"About Image"}
+            />
+            <div className="text-justify pb-10">
+                {description}
+            </div>
+            {
+                button == ""?
+                <></>
+                :
+                <button>
+                    {button}
+                </button>
+            }
         </div>
     );
 }
