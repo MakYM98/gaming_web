f9093ca89bf9c5db59a8b73711f1792e1f72cbb5
YeongMeng <makyeongmeng@gmail.com>
2023-11-24 12:07:56 +0800

    Project Initialization

---
+++ components/home/secondSection.tsx
@@ -0,0 +1,9 @@
+const SecondSection = () => {
+    return ( 
+        <div className="h-full w-full bg-white">
+            
+        </div> 
+    );
+}
+ 
+export default SecondSection;
---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,20 @@
+const NavBar = () => {
+    return ( 
+        <div className="w-3/4 px-10 bg-gray-500/25 rounded-2xl h-10 flex justify-between">
+            {/* Brand Name */}
+            <div className="flex items-center h-full">
+                Yeong Meng
+            </div>
+            {/* Nav Items */}
+            <div>
+                Test
+            </div>
+            {/* Play Button */}
+            <div>
+                Home
+            </div>
+        </div>
+    );
+}
+ 
+export default NavBar;
